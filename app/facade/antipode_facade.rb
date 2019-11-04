e9f32725cbdeabfa8edc9b9ab6a95f929204700c
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-04 10:03:22 -0700

    Create antipode facade

---
+++ app/facade/antipode_facade.rb
@@ -0,0 +1,20 @@
+class AntipodeFacade
+  attr_reader :location
+
+  def initialize(location)
+    @location = location
+  end
+
+  def get_antipode_forecast(location)
+    geocode_service = GeocodeService.new
+    coordinates = geocode_service.location_coordinates(location)
+
+    antipode_service = AntipodeService.new
+    antipode_coordinates = antipode_service.get_antipode(coordinates)
+
+    darksky_service = DarkSkyService.new
+    darksky_service.forecast(antipode_coordinates)
+  end
+
+
+end

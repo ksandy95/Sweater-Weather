b1297477fde5e4365b9bc9601ffe68d8ef92e780
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-04 10:11:39 -0700

    get forecast with facade method

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::AntipodeController < ApplicationController
   def show
     location = params[:location]
     facade = AntipodeFacade.new(location)
+    forecast = facade.get_antipode_forecast(location)
+    binding.pry
   end
 
 end

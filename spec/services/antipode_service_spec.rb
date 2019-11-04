325550d3e57ddbc0001f6d528a853a016528619d
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-04 10:03:06 -0700

    Fix test with updated service method

--- spec/services/antipode_service_spec.rb
+++ spec/services/antipode_service_spec.rb
@@ -7,12 +7,10 @@ RSpec.describe "Antipode Service" do
       service = AntipodeService.new
       coordinates = {:lat=>-22.1044031, :lng=>-65.5967709}
       response = service.get_antipode(coordinates)
-      body = response.body
-      json_d = JSON.parse(body, symbolize_names: true)
 
-      expect(json_d).to be_a Hash
-      expect(json_d[:data][:attributes][:lat]).to eq(22.1044031)
-      expect(json_d[:data][:attributes][:long]).to eq(180.0)
+      expect(response).to be_a Hash
+      expect(response[:lat]).to eq(22.1044031)
+      expect(response[:long]).to eq(180.0)
     end
 
   end

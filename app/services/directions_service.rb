1cdf7f2c6a96b5bbe44cb86b456ca9eea895ed79
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-03 19:23:39 -0700

    Update service and test

--- app/services/directions_service.rb
+++ app/services/directions_service.rb
@@ -2,15 +2,19 @@ class DirectionsService
 
 # get directions from one place to another.
 # get travel time between those places
-  def travel_time(origin, destination)
-    response = conn.get("origin=#{origin}&destination=#{destination}&departure_time=now&key=#{ENV['DIRECTIONS_API']}")
-    body = response.body
-    json = JSON.parse(body, symbolize_names:true)
-    json[:routes][0][:legs][0][:duration]
-  end
 
-  def conn
-    Faraday.get "https://maps.googleapis.com/maps/api/directions/json?"
-  end
+# api is failing to separate after locations are parsed out from initial get conn
+  #
+  # def travel_time(locations)
+  #   response = conn.get("origin=#{locations[:origin]}&destination=#{locations[:destination]}&departure_time=now&key=#{ENV['DIRECTIONS_API']}")
+  #   body = response.body
+  #   binding.pry
+  #   json = JSON.parse(body, symbolize_names:true)
+  #   json[:routes][0][:legs][0][:duration]
+  # end
+  #
+  # def conn
+  #   Faraday.new(url: "https://maps.googleapis.com/maps/api/directions/json?")
+  # end
 
 end
--- spec/services/directions_service_spec.rb
+++ spec/services/directions_service_spec.rb
@@ -4,8 +4,19 @@ RSpec.describe "Directions Service" do
   describe "Getting a connection without error" do
 
     it "can get a connection" do
+      # service = DirectionsService.new
+      # response = service.conn
+      # body = response.body
+      # json = JSON.parse(body, symbolize_names: true)
+      #
+      # expect(json).to be_a Hash
+      # expect(json.count).to eq(3)
+    end
+
+    it "can be split into different methods" do
       service = DirectionsService.new
-      response = service.conn
+      locations = {:origin=>"Boston,MA", :destination=>"Concord,MA"}
+      response = service.travel_time(locations)
       binding.pry
     end
 

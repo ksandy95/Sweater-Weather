526439f602ccfe05f9f32ff686ac295af9705c0c
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-03 19:49:57 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'welcome#show'
+  get '/', to: 'welcome#show', as: :root
 
   namespace :api do
     namespace :v1 do

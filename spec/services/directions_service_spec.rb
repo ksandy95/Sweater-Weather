5ded60b1da132be390aa0b3f602d93b397690999
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-03 19:23:59 -0700

    Update test to come back to later

--- spec/services/directions_service_spec.rb
+++ spec/services/directions_service_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe "Directions Service" do
   describe "Getting a connection without error" do
 
-    it "can get a connection" do
-      # service = DirectionsService.new
-      # response = service.conn
-      # body = response.body
-      # json = JSON.parse(body, symbolize_names: true)
-      #
-      # expect(json).to be_a Hash
-      # expect(json.count).to eq(3)
-    end
-
-    it "can be split into different methods" do
-      service = DirectionsService.new
-      locations = {:origin=>"Boston,MA", :destination=>"Concord,MA"}
-      response = service.travel_time(locations)
-      binding.pry
-    end
+    # it "can get a connection" do
+    #   # service = DirectionsService.new
+    #   # response = service.conn
+    #   # body = response.body
+    #   # json = JSON.parse(body, symbolize_names: true)
+    #   #
+    #   # expect(json).to be_a Hash
+    #   # expect(json.count).to eq(3)
+    # end
+    #
+    # it "can be split into different methods" do
+    #   service = DirectionsService.new
+    #   locations = {:origin=>"Boston,MA", :destination=>"Concord,MA"}
+    #   response = service.travel_time(locations)
+    #   binding.pry
+    # end
 
   end
 end

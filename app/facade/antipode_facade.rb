08642491cf090a6781a053ecaa7eac1fec9a42e7
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-04 11:50:52 -0700

    Notes for instructors. Clump out data for workability and DRY usage. Comments for next steps

--- app/facade/antipode_facade.rb
+++ app/facade/antipode_facade.rb
@@ -5,19 +5,31 @@ class AntipodeFacade
     @location = location
   end
 
-  def get_antipode_forecast(location)
+  def antipode_data(location)
     geocode_service = GeocodeService.new
     coordinates = geocode_service.location_coordinates(location)
 
     antipode_service = AntipodeService.new
     antipode_coordinates = antipode_service.get_antipode(coordinates)
-    binding.pry
+  end
+
+  def get_antipode_forecast(location)
+    antipode_coordinates = antipode_data(location)
+
     darksky_service = DarkSkyService.new
-    darksky_service.forecast(antipode_coordinates)
+    darksky_service.forecast(antipode_coordinates[:data][:attributes])
   end
 
   def parsed_antipode_forecast(location)
-    antipode_location =
+    antipode_location_coordinates = antipode_data(location)
+    coordinates = antipode_location_coordinates[:data][:attributes]
+
+    geocode_service = GeocodeService.new
+    # https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
+    # silly me forgot to get the reverse lookup. We are running out of time for this challenge
+    # but i will continue to work on it by creating the reverse lookup method in
+    # my geocode service file.
+
     body = get_antipode_forecast(location).body
     parsed = JSON.parse(body, symbolize_names: true)
     hash = {search_location: location,

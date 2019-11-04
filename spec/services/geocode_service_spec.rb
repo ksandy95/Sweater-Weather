0dc2f440ef263b11527bda173b66d3a7db5fbee1
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-03 17:44:44 -0700

    Comment out old test for new functionality

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -3,14 +3,21 @@ require 'rails_helper'
 RSpec.describe "Geocode Service" do
 
   describe "Getting a connection with Faraday" do
-    it "Can connect without an error" do
+    # it "Can connect without an error" do
+    #   service = GeocodeService.new
+    #   connection = service.conn
+    #   body = connection.body
+    #   json_d = JSON.parse(body, symbolize_names: true)
+    #   lat_long = json_d[:results][0][:geometry][:location]
+    #   expect(lat_long).to be_a Hash
+    #   expect(lat_long.count).to eq(2)
+    # end
+
+    it "can pass in a location" do
       service = GeocodeService.new
-      connection = service.conn
-      body = connection.body
-      json_d = JSON.parse(body, symbolize_names: true)
-      lat_long = json_d[:results][0][:geometry][:location]
-      expect(lat_long).to be_a Hash
-      expect(lat_long.count).to eq(2)
+      test = service.location_coordinates("Denver,CO")
+      binding.pry
+      expect(test.count).to eq(2)
     end
   end
 end

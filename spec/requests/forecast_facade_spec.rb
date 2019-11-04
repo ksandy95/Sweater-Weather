c13d516e539d25e577a38ff418c375e13d678b4f
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-04 09:19:59 -0700

    Fix testing issue.

--- spec/requests/forecast_facade_spec.rb
+++ spec/requests/forecast_facade_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe "Using Forecast Facade" do
   describe "Getting back a forecast" do
 
     it "Does not error out when called" do
-      
+
       get '/api/v1/forecast?location=denver,co'
 
+      expect(response).to be_successful
     end
 
   end

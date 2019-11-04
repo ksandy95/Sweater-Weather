7786e728bf9657a80d51997953dac6e8dcd857ee
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-04 10:16:47 -0700

    Find error in facade method pry in to fix where things are going wrong

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AntipodeController < ApplicationController
 
   def show
     location = params[:location]
-    facade = AntipodeFacade.new(location)
+    facade = AntipodeFacade.new(params[:location])
     forecast = facade.get_antipode_forecast(location)
     binding.pry
   end

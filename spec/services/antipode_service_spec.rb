30fa8f9dba78c198d7c2cdd818145a525b97b913
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-04 09:50:11 -0700

    update test to be specific about the lat and long i should recieve back

--- spec/services/antipode_service_spec.rb
+++ spec/services/antipode_service_spec.rb
@@ -1,4 +1,3 @@
-# {:lat=>-22.1044031, :lng=>-65.5967709}
 require 'rails_helper'
 
 RSpec.describe "Antipode Service" do
@@ -6,11 +5,14 @@ RSpec.describe "Antipode Service" do
 
     it "can connect without error" do
       service = AntipodeService.new
-      response = service.conn
+      coordinates = {:lat=>-22.1044031, :lng=>-65.5967709}
+      response = service.get_antipode(coordinates)
       body = response.body
       json_d = JSON.parse(body, symbolize_names: true)
 
       expect(json_d).to be_a Hash
+      expect(json_d[:data][:attributes][:lat]).to eq(22.1044031)
+      expect(json_d[:data][:attributes][:long]).to eq(180.0)
     end
 
   end

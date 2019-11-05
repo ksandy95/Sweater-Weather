086eaeb063dc47810bead50b0e742798c25c40d9
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-05 14:04:58 -0700

    Add in api key to parameters

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -2,6 +2,7 @@ class GiphyService
 
   def conn
     Faraday.new(url: "https://api.giphy.com/v1/gifs/search") do |f|
+      f.params['api_key'] = ENV['GIPHY_API']
       f.adapter Faraday.default_adapter
     end
   end

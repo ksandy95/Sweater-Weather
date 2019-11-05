631dc68a5f4f9e1dbddb80c6a17249adc086560a
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-05 14:08:37 -0700

    add content to test

--- spec/services/giphy_service_spec.rb
+++ spec/services/giphy_service_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe "Giphy Service" do
   describe "Getting a connection with Faraday" do
 
     it "does not error out when used" do
-      
+      service = GiphyService.new
+      response = service.get_gif
+
     end
 
   end

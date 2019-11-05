5bd7cbef050345ab62aaba78233fb15116e0b6ae
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-05 13:53:26 -0700

    Create methods to fill facade, get ready for Service to get gifs for each summary

--- app/facade/gif_facade.rb
+++ app/facade/gif_facade.rb
@@ -4,6 +4,21 @@ class GifFacade
   def initialize(location)
     @location = location
   end
-  
+
+  def geocode_coords
+    geocode_service = GeocodeService.new
+    coordinates = geocode_service.location_coordinates(@location)
+  end
+
+  def forecast_data
+    darksky_service = DarkSkyService.new
+    location_forecast = darksky_service.forecast(geocode_coords)
+  end
+
+  def gif_forecast
+    forecast_data
+    parsed = JSON.parse(forecast_data.body, symbolize_names: true)
+    hourly_data_first_10 = parsed[:hourly][:data][0..9]
+  end
 
 end

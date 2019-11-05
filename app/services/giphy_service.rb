f71fca7b1f740ead66bff7942c743607d2d6a0a0
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-05 14:23:26 -0700

    Use postman to get solid connection and break up connection and search methods.

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -1,12 +1,11 @@
 class GiphyService
 
   def get_gif(search)
-    
+    conn.get("/search?q=#{search}&api_key=#{ENV['GIPHY_API']}")
   end
 
   def conn
-    Faraday.new(url: "https://api.giphy.com/v1/gifs/search") do |f|
-      f.params['api_key'] = ENV['GIPHY_API']
+    Faraday.new(url: "https://api.giphy.com/v1/gifs") do |f|
       f.adapter Faraday.default_adapter
     end
   end

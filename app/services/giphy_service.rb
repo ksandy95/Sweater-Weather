f12f0fb7b0f154baac3260c76df7b98f434a79c7
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-05 14:32:06 -0700

    Make service work with the one get_gifs method

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -1,13 +1,7 @@
 class GiphyService
 
   def get_gif(search)
-    conn.get("/search?q=#{search}&api_key=#{ENV['GIPHY_API']}")
-  end
-
-  def conn
-    Faraday.new(url: "https://api.giphy.com/v1/gifs") do |f|
-      f.adapter Faraday.default_adapter
-    end
+    Faraday.get("https://api.giphy.com/v1/gifs/search?q=#{search}&api_key=#{ENV['GIPHY_API']}")
   end
 
 end

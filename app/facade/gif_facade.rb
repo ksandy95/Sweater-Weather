ab8e102fbef9d002ded150938628c3785736229c
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-05 14:00:24 -0700

    Take out pry

--- app/facade/gif_facade.rb
+++ app/facade/gif_facade.rb
@@ -19,7 +19,6 @@ class GifFacade
     forecast_data
     parsed = JSON.parse(forecast_data.body, symbolize_names: true)
     hourly_data_first_10 = parsed[:hourly][:data][0..9]
-    binding.pry
     first_10_summary = hourly_data_first_10.map do |forecast|
                           forecast[:summary]
                        end

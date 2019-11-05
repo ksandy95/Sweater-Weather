695cb0cfe29305c764d238014f4fe247964cbabe
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-05 14:06:20 -0700

    Create test for service connection

---
+++ spec/services/giphy_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+
+RSpec.describe "Giphy Service" do
+  describe "Getting a connection with Faraday" do
+
+    it "does not error out when used" do
+      
+    end
+
+  end
+end

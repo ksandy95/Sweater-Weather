bbab4cf570ecb19ed054013712ba8af6ebe2656c
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 09:45:31 -0700

    route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
-      get '/backgrounds', to: 'backgrounds#show'
+      get '/backgrounds', to: 'background#show'
       get '/antipode', to: 'antipode#show'
     end
   end

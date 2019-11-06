e1c368cf4dbc9ed8870f0704fd69eae4c796babb
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 10:42:37 -0700

    Change user creation without using create! instead using new and save

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    user = User.create!(user_params, api_key: ApiKey.new.key)
+    user = User.new(email: user_params[:email],
+                     password: user_params[:password],
+                     password_confirmation: user_params[:password_confirmation],
+                     api_key: ApiKey.new.key)
+    user.save
     render json: UserSerializer.new(user)
   end
 

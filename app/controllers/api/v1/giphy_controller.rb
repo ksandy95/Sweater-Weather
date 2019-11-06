d2c40820b3e3a3024fa79041acf534d4e96257f1
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-05 20:59:02 -0700

    Update giphy controller

--- app/controllers/api/v1/giphy_controller.rb
+++ app/controllers/api/v1/giphy_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::GiphyController < ApplicationController
 
   def show
-    gif_data = GifFacade.new(params[:location]).gif_forecast
-    render json: GiphySerializer.new(gif_data).giphy_return
+    gif_data = GifFacade.new(params[:location])
+    render json: GiphySerializer.new(gif_data)
   end
-
 end

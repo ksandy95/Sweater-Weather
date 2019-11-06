7c501c34a4439b28b9acaf2a40ab21ac8f95b768
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 10:14:58 -0700

    add rendering dream drive now that model tests pass

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.create!(params['email'], params['password'], params['password_confirmation'])
+    response = UserService.new(user)
+    render json: UserSerializer.new(response)
   end
 
 end

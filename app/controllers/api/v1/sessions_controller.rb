d59678b9842f5846b3818ee1f27c224b5dfba88e
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 11:21:29 -0700

    find, authenticate and render user and status

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-
+    user = User.find_by(email: params[:email])
+    user.authenticate(params[:password])
+    render json: UserSerializer.new(user), status: 200
   end
 
 end

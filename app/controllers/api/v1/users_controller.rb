dc009cd8491b6593ca163862d3fcc8124e58b2d6
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 10:00:23 -0700

    dream drive creating a user with passed in user information

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-
+    user = User.create!(params['email'], params['password'], params['password_confirmation'])
   end
-  
+
 end

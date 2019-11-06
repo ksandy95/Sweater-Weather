e52a5fcc8ac307b6a5281e1fcb1d0d4a43d7a5e3
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 10:47:05 -0700

    remove test to check api addition to user

--- spec/requests/account_creation_request_spec.rb
+++ spec/requests/account_creation_request_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe "Creating an account" do
 
       result = JSON.parse(response.body)
 
-      expect(response).to have_status(200)
       expect(result).to be_a(Hash)
       expect(result['api_key']).to eq('jgn983hy48thw9begh98h4539h4')
     end

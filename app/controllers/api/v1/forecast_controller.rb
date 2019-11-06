3850ff298959a389f742f3499f65815956ca8bef
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 13:46:52 -0700

    fix user save

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
 
-  # shows forecast data for a specific location
   def show
     forecast = ForecastFacade.new(params[:location]).get_forecast(params[:location])
     render json: ForecastSerializer.new(Forecast.new(forecast))
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,11 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    user = User.new(email: user_params[:email],
-                     password: user_params[:password],
-                     password_confirmation: user_params[:password_confirmation],
+    user = User.new(email: params[:email],
+                     password: params[:password],
                      api_key: ApiKey.new.key)
     user.save
     render json: UserSerializer.new(user)
-  end
-
-  def user_params
-    params.permit(params[:email], params[:password], params[:password_confirmation])
   end
 
 end

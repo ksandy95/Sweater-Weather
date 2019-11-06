794fb13bee7447738088982765563c9003fa97fc
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 14:40:00 -0700

    spacing

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,7 +3,6 @@
 class Api::V1::ForecastController < ApplicationController
   def show
     forecast = ForecastFacade.new(params[:location]).get_forecast(params[:location])
-
     render json: ForecastSerializer.new(Forecast.new(forecast))
   end
 end

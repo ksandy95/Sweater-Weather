19f0b8421952667ea0b7621f812c8b17f2b59b7e
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 10:08:23 -0700

    add api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
-  validates_presence_of :password_digest
+  validates_presence_of :password_digest, :api_key
   has_secure_password
 
 end

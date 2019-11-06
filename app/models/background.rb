179dcccdb79676c4bef5120743ac2ee616b2c259
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 09:44:37 -0700

    Create model

---
+++ app/models/background.rb
@@ -0,0 +1,10 @@
+class Background
+  attr_reader :id, :url, :data
+
+  def initialize(data)
+    @data = data[:results][0]
+    @id = data[:results][0][:id]
+    @url = data[:results][0][:urls][:raw]
+  end
+
+end

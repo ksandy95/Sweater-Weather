8842c4798052bea5fe5933206e689f791c40ee02
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 10:47:51 -0700

    api is nil, add back test

--- spec/requests/account_creation_request_spec.rb
+++ spec/requests/account_creation_request_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe "Creating an account" do
 
       result = JSON.parse(response.body)
 
+      expect(response).to have_status(200)
       expect(result).to be_a(Hash)
       expect(result['api_key']).to eq('jgn983hy48thw9begh98h4539h4')
     end

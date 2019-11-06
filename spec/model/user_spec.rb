3644d01dfa1832d46da947a1a623843a332a5263
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 10:10:52 -0700

    add user model test

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "User Model" do
+  describe User, type: :model do
+
+    it 'exists' do
+      user = User.new( email: 'email', password: 'test', password_confirmation: 'test' )
+      expect(user).to be_a(User)
+    end
+
+    context 'Validations' do
+      it { should validate_presence_of(:email) }
+      it { should validate_uniqueness_of(:email) }
+      it { should validate_presence_of(:password) }
+      it { should validate_presence_of(:api_key) }
+    end
+
+  end
+end

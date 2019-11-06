e9e569ed6c1bda281ba6d90bbf9f863dd4d12b0b
Kelly Sandeson <kellysandeson@gmail.com>
2019-11-06 10:36:59 -0700

    Add in api key model to attach to user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    user = User.create!(user_params)
+    user = User.create!(user_params, api_key: ApiKey.new.key)
     render json: UserSerializer.new(user)
   end
 
